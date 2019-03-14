b6c8fb265e51f29e95caa30a32f735530040cc73
Berner Roy <r.p.berner@me.com>
2019-03-14 22:14:16 +0100

    Initial Commit

---
+++ Conc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Conc
+//
+//  Created by Roy Berner on 14/03/2019.
+//  Copyright © 2019 Roy Berner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
